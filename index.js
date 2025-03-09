07ec4fa6419a0629812ddefb2ef522160db1ae86
HuntsberryS <huntsberry.shiloh@gmail.com>
2025-03-09 19:44:36 -0400

    remove save tab and send info to firebase for later access

--- index.js
+++ index.js
@@ -1,55 +1,22 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js"
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-app.js"
+import { getDatabase,
+         ref,
+         push } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-database.js"
 
 const firebaseConfig = {
-    
+    databaseURL: "https://keep-calm-and-carry-on-e73a1-default-rtdb.europe-west1.firebasedatabase.app/"
 }
 
 const app = initializeApp(firebaseConfig)
+const database = getDatabase(app)
+const referenceInDB = ref(database, "products")
 
-let myLeads = []
-const inputEl = document.getElementById("input-el")
-const inputBtn = document.getElementById("input-btn")
-const ulEl = document.getElementById("ul-el")
-const deleteBtn = document.getElementById("delete-btn")
-const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
-const tabBtn = document.getElementById("tab-btn")
+const productNameInputField = document.getElementById("product-name-input")
+const productValueInputField = document.getElementById("product-value-input")
+const submitButton = document.getElementById("submit-button")
 
-if (leadsFromLocalStorage) {
-    myLeads = leadsFromLocalStorage
-    render(myLeads)
-}
-
-tabBtn.addEventListener("click", function(){    
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-        myLeads.push(tabs[0].url)
-        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
-        render(myLeads)
-    })
-})
-
-function render(leads) {
-    let listItems = ""
-    for (let i = 0; i < leads.length; i++) {
-        listItems += `
-            <li>
-                <a target='_blank' href='${leads[i]}'>
-                    ${leads[i]}
-                </a>
-            </li>
-        `
-    }
-    ulEl.innerHTML = listItems
-}
-
-deleteBtn.addEventListener("dblclick", function() {
-    localStorage.clear()
-    myLeads = []
-    render(myLeads)
-})
-
-inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value)
-    inputEl.value = ""
-    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
-    render(myLeads)
+submitButton.addEventListener("click", function() {
+    productNameInputField.value
+    push(referenceInDB, productNameInputField.value)
+    productNameInputField.value = ""
 })
